4b5cf9b1a56fef38745f8aaecaf13f726cf90709
matthew lee <laughew@gmail.com>
2023-07-22 15:15:47 -0700

    ok i think im happy with this

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,39 +16,44 @@
 
 		daySpan.innerHTML = dayNames[now.getDay()] + ' ' + stage;
 
+		let period = hours >= 12 ? 'p.m.' : 'a.m.';
+
 		hours = hours % 12;
+		hours = hours ? hours : 12;
 		hours = (hours < 10 ? '0' : '') + hours;
 
 		let minutes = now.getMinutes();
 		minutes = (minutes < 10 ? '0' : '') + minutes;
-
-		let period = hours / 12 > 1 ? 'p.m.' : 'a.m.';
 
 		timeSpan.innerHTML = hours + ':' + minutes + period;
 	});
 </script>
 
 <div class="container">
-	<div class="qna">
-		<p class="right"><strong>when</strong></p>
-		<p id="when">
+	<div class="grid">
+
+		<p class="centered row one"><strong>when</strong></p>
+		<p class="row one indented">
 			it's <span id="time">--:--</span><br />
-			on a <span id="day">------</span>, and
+			on a <span id="day">-------</span>, and
 		</p>
-		<p class="right"><strong>where</strong></p>
-		<p>
+
+		<p class="centered row two"><strong>where</strong></p>
+		<p class="row two indented">
 			you've stumbled upon <a href="/">matthewjl.xyz</a>,<br />
 			the personal website belonging to
 		</p>
-		<p class="right"><strong>who</strong></p>
-		<div>
+
+		<p class="centered row three"><strong>who</strong></p>
+		<div class="row indented">
 			<h1>matthew j lee</h1>
 			<br />
-			whose online presence is expressed through
+			<div class="row three">whose online presence is expressed through</div>
 		</div>
-		<p class="right"><strong>what</strong></p>
-		<div class="split-cell">
-			<div>
+
+		<p class="centered row four"><strong>what</strong></p>
+		<div class="split-cell indented" indented>
+			<div class="row four">
 				<h2><a href="/">writing</a></h2>
 				<h2><a href="/">work</a></h2>
 				<h2><a href="/">credentials</a></h2>
@@ -56,18 +61,22 @@
 				<h2><a href="/">thoughts</a></h2>
 				<h2><a href="/">personal info</a></h2>
 			</div>
-			<div class="image-wrapper">
+			<div class="image-wrapper row zero">
 				<img src="/images/gingko.png" alt="3 gingko leaves" />
 			</div>
 		</div>
-		<p class="right"><strong>why</strong></p>
-		<p>
+
+		<p class="centered row five"><strong>why</strong></p>
+		<p class="row five indented">
 			in the hope of<br />
 			maintaining a personal record,<br />
 			connecting from afar,<br />
 			nurturing a personal space,<br />
 			& learning/creating continuously.
 		</p>
+
+		<div class="right-column"></div>
+
 	</div>
 </div>
 
@@ -78,14 +87,40 @@
 		justify-content: center;
 	}
 
-	.qna {
+	.grid {
 		display: grid;
-		grid-template-columns: 1fr 7.2fr;
+		grid-template-columns: 0.8fr 1fr minmax(0px, 0.6fr);
 		width: 100%;
-		max-width: 720px;
-		gap: 0.8rem 20%;
+		gap: 0.4rem;
 	}
 
+	.row {
+		opacity: 0;
+		animation: fadein 1s ease-in-out forwards;
+	}
+
+	.one {
+		animation-delay: 0.8s
+	}
+
+	.two {
+		animation-delay: 1.2s
+	}
+
+	.three {
+		animation-delay: 1.6s
+	}
+
+	.four {
+		animation-delay: 2s
+	}
+
+	.five {
+		animation-delay: 2.4s
+	}
+	
+	
+	
 	h1 {
 		font-size: 4rem;
 		line-height: 4rem;
@@ -120,8 +155,26 @@
 		max-width: 100%;
 	}
 
-	.right {
-		text-align: right;
+	.centered {
+		text-align: center;
+	}
+
+	.right-column {
+		grid-column: 3;
+		grid-row: 1 / 5;
+	}
+
+	.indented {
+		margin-left: 12%;
+	}
+
+	@keyframes fadein {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
 	}
 	
 </style>
